2c67512c4e12854e5ce2feace982802957e87ca7
Dheeraj Sharma <dheeraj9410096874@gmail.com>
2025-03-21 17:17:33 +0530

    [Dheeraj Sharma] Add: UC4 implemented.

--- Pincode.js
+++ Pincode.js
@@ -1,5 +1,6 @@
-// Update the regex to restrict alphabets or special characters at the end
-const pincodePattern = /^[0-9]{6}$/;
+// Update the regex to allow optional space in the middle and restrict alphabets or special characters at the end
+const pincodePattern = /^([0-9]{3}\s?[0-9]{3})$/;
+// const pincodePattern = /^[0-9]{6}$/;
 // const pincodePattern = /^[0-9]{6}$/;
 // const pincodePattern = /^400088$/;
 
